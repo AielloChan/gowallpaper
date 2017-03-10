d60fcdb94e9da0c2abbb2e2b3a68d915bdf1f46e
Aiello Chan <Aiello.Chan@Gmail.com>
2017-03-10 13:35:57 +0800

    Add new source

---
+++ picsource/bing.go
@@ -0,0 +1,71 @@
+package picsource
+
+import (
+	"../tools"
+	"encoding/json"
+	"fmt"
+	"os"
+	"path"
+	"strings"
+)
+
+const (
+	BING_API_URL = `http://www.bing.com/HPImageArchive.aspx?format=js&idx=0&n=8`
+)
+
+type images struct {
+	Images []image `json:"images"`
+}
+
+type image struct {
+	URL       string `json:"url"`
+	Copyright string `json:"copyright"`
+}
+
+// Bing provide pictures
+func Bing(fileDir string) (string, error) {
+
+	fmt.Println("[1/4]Checking directory...")
+	os.MkdirAll(fileDir, 0777)
+
+	fmt.Println("[2/4] Loadding API...")
+	jsonData, err := tools.Fetch(BING_API_URL)
+	if err != nil {
+		os.Exit(2)
+	}
+
+	var jsonObj images
+	fmt.Println("Parse json string...")
+	if err := json.Unmarshal(jsonData, &jsonObj); err != nil {
+		fmt.Println("Parse json string failed.", err)
+		return "", err
+	}
+
+	currentItem := jsonObj.Images[0]
+	picURL := "http://www.bing.com" + currentItem.URL
+
+	fmt.Println(currentItem.Copyright)
+
+	fileName := currentItem.Copyright[:strings.Index(currentItem.Copyright, "，")]
+	fileName += ".jpg"
+
+	filePath := path.Join(tools.GetCurrentDirectory(), fileDir, fileName)
+	if !tools.CheckPathExists(filePath) {
+		fmt.Println("[3/4] Downlouding picture...")
+		imageData, err := tools.Fetch(picURL)
+		if err != nil {
+			os.Exit(2)
+		}
+
+		fmt.Println("[4/4] Save file...")
+		if err := tools.WriteFile(filePath, imageData); err != nil {
+			fmt.Println(err)
+			os.Exit(2)
+		}
+	} else {
+		fmt.Println("[3/4] File already exists...")
+		fmt.Println("[4/4] Check file...")
+	}
+
+	return filePath, nil
+}
